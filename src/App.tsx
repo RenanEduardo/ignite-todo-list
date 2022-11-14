9ce74b9bf3dfb7a30190140cdfb1d6c0281ae892
Renan Eduardo <renaneduardo1@hotmail.com>
2022-11-14 20:55:48 -0300

    feat: create skeleton and Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { Header } from "./components/Header";
+import "./globals.css";
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <div>
+      <Header />
+      <main>
+        <div>
+          <input type="text" />
+          <button>Criar</button>
+        </div>
+      </main>
+      <footer>
+        <div>
+          <span>tarefas criadas 0</span>
+          <span>Concluidas 0</span>
+        </div>
+      </footer>
+    </div>
   )
 }
 
